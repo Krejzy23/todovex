a84cece6866df92b23717d7dc9dce4153504d510
Aleš Krejzl <ales.krejzl@gmail.com>
2024-06-28 16:20:38 +0200

    add database

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Todovex</h1>
+      <Button>Enter</Button> 
     </main>
   );
 }
